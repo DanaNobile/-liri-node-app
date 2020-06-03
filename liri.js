a321d6248357e6d860b8439a2da4be275700483f
Dana Nobile <danagnobile@gmail.com>
2020-06-03 11:47:12 -0400

    Hav movie function implemnted for search

--- liri.js
+++ liri.js
@@ -11,10 +11,12 @@ var Spotify = require("node-spotify-api");
 var spotifyKeyInfo = require("./keys.js");
 // let keys = require("./assets/keys.js");
 
-console.log(" \n Hi, I'm Liri! What do you want to do today? \n Choose from the following: 'concert-this', 'spotify-this-song', 'movie-this' or 'do-what-it-says'");
-
 var userInput = process.argv;
 var inputTopic = process.argv[2];
+
+// console.log(" \n Hi, I'm Liri! What do you want to do today? \n Choose from the following: 'concert-this', 'spotify-this-song', 'movie-this' or 'do-what-it-says'");
+
+
 
 
 switch (inputTopic) {
@@ -35,7 +37,7 @@ switch (inputTopic) {
         break;
 }
 
-// This code required to import the keys.js file and store it in a variable.
+
 
 
 
@@ -56,6 +58,7 @@ function songInfo() {
             songName += userInput[i];
         }
     }
+
     var spotify = new Spotify({
         id: spotifyKeyInfo["spotify"].id,
         secret: spotifyKeyInfo["spotify"].secret
@@ -82,54 +85,51 @@ function songInfo() {
 
 
 
-    // =========================== Movie =========================== //
-    // var axios = require("axios");
+// =========================== Movie =========================== //
 
-    // var nodeArgs = process.argv;
+function movieInfo() {
+    var movieName = "";
+    for (var i = 3; i < userInput.length; i++) {
+        if (i > 3 && i < userInput.length) {
+            movieName = movieName + "+" + userInput[i];
+        }
+        else {
+            movieName += userInput[i];
+        }
+    }
 
-    // var movieName = "";
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
 
-    // for (var i = 2; i < nodeArgs.length; i++) {
-
-    //     if (i > 2 && i < nodeArgs.length) {
-    //         movieName = movieName + "+" + nodeArgs[i];
-    //     } else {
-    //         movieName += nodeArgs[i];
-
-    //     }
-    // }
-
-    // var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    // console.log(queryUrl);
-
-    // axios.get(queryUrl).then(
-    //     function (response) {
-    //         console.log("Movie Title: " + response.data.Title);
-    //         console.log("Release Year: " + response.data.Year);
-    //         console.log("IMDB Rating: " + response.data.imdbRating);
-    //         console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-    //         console.log("Production Country: " + response.data.Country);
-    //         console.log("Language: " + response.data.Language);
-    //         console.log("Plot: " + response.data.Plot);
-    //         console.log("Actors: " + response.data.Actors);
-    //     })
-    //     .catch(function (error) {
-    //         if (error.response) {
-    //             // The request was made and the server responded with a status code
-    //             // that falls out of the range of 2xx
-    //             console.log("---------------Data---------------");
-    //             console.log(error.response.data);
-    //             console.log("---------------Status---------------");
-    //             console.log(error.response.status);
-    //             console.log("---------------Status---------------");
-    //             console.log(error.response.headers);
-    //         } else if (error.request) {
-    //             // The request was made but no response was received
-    //             // `error.request` is an object that comes back with details pertaining to the error that occurred.
-    //             console.log(error.request);
-    //         } else {
-    //             // Something happened in setting up the request that triggered an Error
-    //             console.log("Error", error.message);
-    //         }
-    //         console.log(error.config);
-    //     });
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log("Movie Title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Production Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
